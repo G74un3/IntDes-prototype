efb0f0c3209e8e4fced297d175555a1e54e74289
Borimino <markusandreassen1994@gmail.com>
2014-03-02 15:51:02 +0100

    Changed the timings on the wheel

--- js/wheel.js
+++ js/wheel.js
@@ -2,13 +2,13 @@ var rot=0;
 
 function click_position()
 {
-	var interval=setInterval(function() {change_position()}, 10);
-	setTimeout(function() {window.clearInterval(interval)}, 980);
+	var interval=setInterval(function() {change_position()}, 1*30);
+	setTimeout(function() {window.clearInterval(interval)}, 31*30);
 }
 
 function change_position()
 {
-	rot++;
+	rot += 3;
 	var s="wheel";
 	document.getElementById(s).style.transform="rotate("+rot+"deg)";
 	document.getElementById(s).style.WebkitTransform="rotate("+rot+"deg)";
